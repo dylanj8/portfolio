32ab8acded578d7cb6c1823126e300b1e91488a1
dylanj8 <110931973+dylanj8@users.noreply.github.com>
2023-11-14 13:53:34 +1100

    "changed about"

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,21 +6,21 @@ const About = () => {
       <h1 className="text-4xl text-center font-bold text-gray-800">About</h1>
 
       <p className="flex flex-wrap mt-2 text-base text-stone-500">
-        Hello, I'm Dylan, an aspiring web developer driven by a deep passion for
-        creating dynamic and user-friendly websites. Although I'm new to the
-        industry, I am dedicated to continuously expanding my skills and staying
-        abreast of the latest web development trends. Along with a solid
-        foundation in HTML, CSS, JavaScript and React, I also have experience in
-        TypeScript, Mongodb, Express, Firebase, and NextJS. I'm eager to
-        leverage my technical knowledge to bring captivating designs to life. My
-        enthusiasm for web development extends beyond the technical aspects. I
-        thrive on problem-solving, collaborating with teams, and turning
-        innovative ideas into reality. I believe in the power of user-centric
-        design and strive to create intuitive interfaces that provide seamless
-        experiences. While I may not have professional experience yet, I am open
-        to opportunities to contribute and grow as a web developer. I am
-        confident that my determination, eagerness to learn, and strong work
-        ethic will enable me to make a meaningful impact.
+        Hello, I'm Dylan, a web developer driven by a deep passion for creating
+        dynamic and user-friendly websites. Although I'm new to the industry, I
+        am dedicated to continuously expanding my skills and staying abreast of
+        the latest web development trends. Along with a solid foundation in
+        HTML, CSS, JavaScript and React, I also have experience in TypeScript,
+        Mongodb, Express, Firebase, and NextJS. I'm eager to leverage my
+        technical knowledge to bring captivating designs to life. My enthusiasm
+        for web development extends beyond the technical aspects. I thrive on
+        problem-solving, collaborating with teams, and turning innovative ideas
+        into reality. I believe in the power of user-centric design and strive
+        to create intuitive interfaces that provide seamless experiences. While
+        I may not have professional experience yet, I am open to opportunities
+        to contribute and grow as a web developer. I am confident that my
+        determination, eagerness to learn, and strong work ethic will enable me
+        to make a meaningful impact.
       </p>
     </div>
   );
